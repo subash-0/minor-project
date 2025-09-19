853ee4039eadef1e315499b0964d1204d760823f
Subash Kumar Yadav <47708841+subash-0@users.noreply.github.com>
2025-09-19 18:26:13 +0545

    Update app.py

--- model/app.py
+++ model/app.py
@@ -134,5 +134,5 @@ def colorize():
 
 # Run the Flask application
 if __name__ == '__main__':
-     port = int(os.environ.get("PORT", 3030))  # Use Render's PORT env or default 5000
+     port = int(os.environ.get("PORT", 5000))  # Use Render's PORT env or default 5000
     app.run(host="0.0.0.0", port=port)
